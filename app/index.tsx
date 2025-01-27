0c75cb17d33e46e5509217687b7c8678879f8455
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-27 21:32:59 +0530

    Add shopping list functionality with input handling and dynamic rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,60 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  {
+    id: "1",
+    name: "Coffee",
+  },
+  {
+    id: "2",
+    name: "Tea",
+  },
+  {
+    id: "3",
+    name: "Milk",
+  },
+];
 
 export default function App() {
+  const [value, setValue] = useState<string>("");
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toTimeString(), name: value.trim() },
+        ...shoppingList,
+      ];
+
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted={true} />
-      <ShoppingListItem name="Milk" isCompleted />
+      <TextInput
+        placeholder="E.g. Coffee"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
     </View>
   );
 }
@@ -17,6 +63,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
