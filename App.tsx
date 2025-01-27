293390df7ef357bd8f29b12e2d3310f38ca9b92f
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-27 14:40:44 +0530

    Add delete confirmation alert to handleDelete function in App component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good.",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
         <TouchableOpacity
-          onPress={() => {
-            console.log("Pressed");
-          }}
+          onPress={handleDelete}
           activeOpacity={0.8}
           style={styles.button}
         >
