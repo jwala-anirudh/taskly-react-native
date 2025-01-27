777c5c0f777e12cc148ff93e8ab6a2eddd8bdfd5
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-27 20:02:52 +0530

    Add Counter and Idea screens; update index to include navigation links

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
+      {/* Navigation to screens work even if we have not defined them here, since its using file based routing */}
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StyleSheet, View } from "react-native";
 
 import { ShoppingListItem } from "../components/ShoppingListItem";
@@ -6,6 +7,12 @@ import { theme } from "../theme";
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to counter
+      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted={true} />
       <ShoppingListItem name="Milk" isCompleted />
