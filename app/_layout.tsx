5951da131382c31eedd06cffbaa1eea029a0e297
Anirudh Jwala <anirudhsai2@gmail.com>
2025-01-27 20:13:59 +0530

    Add Counter and Idea screens to layout with modal presentation options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,18 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-      {/* Navigation to screens work even if we have not defined them here, since its using file based routing */}
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "Idea", presentation: "modal" }}
+      />
     </Stack>
   );
 }
